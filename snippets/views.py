562baa164c42ffd85c559b4925c4964551fff612
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-03-02 09:41:53 +0900

    Add debug version endpoint

--- snippets/views.py
+++ snippets/views.py
@@ -77,11 +77,11 @@ def diagnosis_result(request, pk: int):
         "build": ["Day1: ペイン選定", "Day2: 1画面プロト", "Day3: 使う人1人", "Day4: 改善", "Day5: 課金ポイント", "Day6: LP", "Day7: 初期募集"],
     }[d.result_type]
 
-    return render(
-        request,
-        "snippets/diagnosis_result.html",
-        {"d": d, "is_premium": is_premium, "free_action": free_action, "premium_roadmap": premium_roadmap},
-    )
+    return render(request, "snippets/diagnosis_result.html", {
+        "d": d,
+        "is_premium": is_premium,
+        "free_action": free_action,
+    })
 
 @csrf_exempt
 def stripe_webhook(request):
@@ -258,3 +258,8 @@ def premium_download_stable(request):
         content_type="application/pdf",
     )
 
+def debug_version(request):
+    return HttpResponse("VERSION: 2026-03-02-02", status=200)
+
+def debug_version(request):
+    return HttpResponse("VERSION=2026-03-02-03", status=200)
