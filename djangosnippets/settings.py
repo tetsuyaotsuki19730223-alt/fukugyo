7b9549b79e07c06d955a20cb99566d0ca7ddbd83
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-03-15 19:13:23 +0900

    deploy fix

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
@@ -91,8 +91,6 @@ WSGI_APPLICATION = 'djangosnippets.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-import os
-from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
