39b965b26fc9552aab664c35d7c27c86c9dafc6a
Tetsuya Otsuki <tetsuyaotsuki19730223@gmail.com>
2026-03-16 21:18:07 +0900

    update

--- snippets/views/landing_views.py
+++ snippets/views/landing_views.py
@@ -1,2 +1,9 @@
+from django.shortcuts import render
+
+
+def landing(request):
+    return render(request, "snippets/landing.html")
+
+
 def diagnosis_lp(request):
     return render(request, "snippets/diagnosis_lp.html")
